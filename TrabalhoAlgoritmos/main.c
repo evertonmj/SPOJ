fde7191a46d8b2643fab08bb1335cf3eea58fead
Everton Mendonça <everton@MacBook-Pro-de-Everton.local>
2014-11-21 10:02:33 -0300

    Initial Commit

---
+++ TrabalhoAlgoritmos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TrabalhoAlgoritmos
+//
+//  Created by Everton Mendonça on 21/11/14.
+//  Copyright (c) 2014 Everton Mendonça. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
